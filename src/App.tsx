1c49508922a42ca4d18d3228456fdb51b4e8fa84
yatin-07 <nish07ravenclaw@gmail.com>
2021-07-22 14:08:58 +0530

    styles for button

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import { DefaultRootState } from "react-redux";
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from './Actions';
 import { initialStateType } from './Reducers/counter';
+import Button from '@material-ui/core/Button';
+import './App.css';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+
 
 function App() {
   const incCounters = useSelector((state: initialStateType) => state.inc);
@@ -10,10 +15,27 @@ function App() {
   //console.log(DefaultRootState);
   return (
     <div className="App">
+      <header className="App-header">
+      <h1>Simple Redux Counter</h1>
       <h1>Counter = {incCounters}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
-      
+      <div>
+      <Button className="button"
+      onClick={() => dispatch(increment())} 
+      startIcon={<AddIcon/>}
+      size = "large"
+      variant="contained"
+      color="primary"
+      ></Button>
+      <Button className="button" 
+      onClick={() => dispatch(decrement())}
+      startIcon={<RemoveIcon/>}
+      size = "large"
+      variant="contained"
+      color="primary">
+      </Button>
+      </div>
+     
+      </header>
     </div>
   );
 }
